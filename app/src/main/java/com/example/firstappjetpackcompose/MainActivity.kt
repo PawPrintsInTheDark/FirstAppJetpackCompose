d697527270016e75816552261b4d4a8605719ec7
Cat <an.kyt.ot15@gmail.com>
2025-01-06 11:37:29 +0500

    State

--- app/src/main/java/com/example/firstappjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstappjetpackcompose/MainActivity.kt
@@ -4,106 +4,133 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalFoundationApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val employees = listOf(
-            Employee("Иван", "Инженер"), Employee("Петр", "Врач"),
-            Employee("Сергей", "Программист"), Employee("Алексей", "Учитель"),
-            Employee("Анна", "Инженер"), Employee("Мария", "Врач"),
-            Employee("Дмитрий", "Программист"), Employee("Елена", "Учитель"),
-            Employee("Олег", "Инженер"), Employee("Татьяна", "Врач"),
-            Employee("Андрей", "Программист"), Employee("Светлана", "Учитель"),
-            Employee("Максим", "Инженер"), Employee("Ксения", "Врач"),
-            Employee("Анастасия", "Программист"), Employee("Игорь", "Учитель"),
-            Employee("Виктор", "Инженер"), Employee("Наталья", "Врач"),
-            Employee("Роман", "Программист"), Employee("Екатерина", "Учитель"),
-            Employee("Юлия", "Инженер"), Employee("Станислав", "Врач"),
-            Employee("Александр", "Программист"), Employee("Ольга", "Учитель"),
-            Employee("Денис", "Инженер"), Employee("Марина", "Врач"),
-            Employee("Владимир", "Программист"), Employee("Тимур", "Учитель"),
-            Employee("Людмила", "Инженер")
-        )
-
-
-        val sortedEmployees = employees.shuffled()
-        val groups = sortedEmployees.groupBy { it.position }
 
         setContent {
-            val listState = rememberLazyListState()
-            val coroutineScope = rememberCoroutineScope()
+            var language by rememberSaveable {
+                mutableStateOf("ru")
+            }
 
-            LazyColumn(
-                state = listState,
-                contentPadding = PaddingValues(6.dp)
-            ) {
-                item {
-                    Text(
-                        text = "В конец", Modifier
-                            .padding(8.dp)
-                            .background(Color.DarkGray)
-                            .padding(6.dp)
-                            .clickable {
-                                coroutineScope.launch {
-                                    listState.animateScrollToItem(employees.size-1)
-                                }
-                            }, fontSize = 28.sp, color = Color.White
+            fun toggleLanguage() {
+                language = if (language == "ru") "en" else "ru"
+            }
+
+            fun getProducts(): List<String> {
+                return if (language == "ru") {
+                    listOf(
+                        "Яблоко",
+                        "Банан",
+                        "Апельсин",
+                        "Груша",
+                        "Виноград",
+                        "Киви",
+                        "Манго",
+                        "Ананас",
+                        "Персик",
+                        "Черника",
+                        "Арбуз",
+                        "Лимон"
                     )
-                }
-                groups.forEach { (type, name) ->
-                    stickyHeader {
-                        Text(
-                            text = type,
-                            fontSize = 32.sp,
-                            fontWeight = FontWeight.Bold,
-                            color = Color.White,
-                            modifier = Modifier
-                                .background(Color.Magenta)
-                                .padding(6.dp)
-                                .fillParentMaxWidth()
-                        )
-                    }
-                    items(name) { emploee ->
-                        Text(text = emploee.firstName, Modifier.padding(6.dp), fontSize = 32.sp)
-                    }
-                }
-                item {
-                    Text(
-                        text = "В начало", Modifier
-                            .padding(8.dp)
-                            .background(Color.DarkGray)
-                            .padding(6.dp)
-                            .clickable {
-                                coroutineScope.launch {
-                                    listState.animateScrollToItem(0)
-                                }
-                            }, fontSize = 28.sp, color = Color.White
+                } else {
+                    listOf(
+                        "Apple",
+                        "Banana",
+                        "Orange",
+                        "Pear",
+                        "Grapes",
+                        "Kiwi",
+                        "Mango",
+                        "Pineapple",
+                        "Peach",
+                        "Blueberry",
+                        "Watermelon",
+                        "Lemon"
                     )
                 }
             }
+
+
+            fun getTitle(): String {
+                return if (language == "ru") "Список продуктов" else "List of products"
+            }
+
+            fun getSwitchText(): String {
+                return if (language == "ru") "Сменить язык" else "Switch language"
+            }
+            Column(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = getTitle(),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 24.sp,
+                    color = Color.White,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color.DarkGray)
+                        .padding(12.dp)
+
+                )
+                LazyColumn(
+                    Modifier.background(Color.LightGray).height(130.dp).padding(4.dp),
+                ) {
+                    items(getProducts()) { product ->
+                        Text(
+                            text = product,
+                            fontSize = 14.sp,
+                            textAlign = TextAlign.Center,
+                            fontWeight = FontWeight.Bold,
+                            modifier = Modifier
+                                .padding(horizontal = 8.dp, vertical = 2.dp)
+                                .fillMaxWidth()
+                                .background(Color.White, shape = RoundedCornerShape(20.dp))
+                                .padding(2.dp)
+
+                        )
+                    }
+                }
+                Text(
+                    text = getSwitchText(),
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier
+                        .clickable { toggleLanguage() }
+                        .padding(vertical = 16.dp)
+                )
+            }
+
+
         }
     }
 }
 
-data class Employee(
-    val firstName: String, val position: String
-)
+
